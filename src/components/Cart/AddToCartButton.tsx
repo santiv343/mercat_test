d2ac89f98044d200b4c8d2d5094a4f5a193b3f02
Santiago Varacca <santiagovaracca@gmail.com>
2023-09-07 00:32:18 -0300

    remove unused components

--- src/components/Cart/AddToCartButton.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AddToCartButton() {
-  return ;
-}
-
-export default AddToCartButton;
--- src/redux/services/products/productsApi.ts
+++ src/redux/services/products/productsApi.ts
@@ -1,7 +1,5 @@
-// Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// Define a service using a base URL and expected endpoints
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
